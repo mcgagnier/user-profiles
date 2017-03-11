1b347b9df152286149d51fa1c5774f3c5eb56c96
mcgagnier <mcgagnier@yahoo.com>
2017-03-11 13:42:45 -0700

    controllers set up

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 var cors = require('cors');
 var port = 8086;
-
+var config = require('./config.js');
+var profileCtrl = require('./controllers/profileCtrl');
+var userCtrl = require('./controllers/userCtrl');
 var corsOptions = {
     origin: 'http://localhost:8086'
 };
@@ -12,3 +14,13 @@ var corsOptions = {
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(session({secret: config.sessionSecret}))
+
+
+
+
+
+
+app.listen(port, function(){
+  console.log("Listeing on port ", port, "Is this thing working");
+});
